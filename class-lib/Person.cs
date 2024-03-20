f1ec5d45b29fc6b4912c4f809b5b621b270e6931
Vamsi Krishna D <vamsi@bundlen.com>
2024-03-20 10:14:43 +0530

    initial changes

---
+++ class-lib/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace class_lib;
+
+public class Person
+{
+    public string Name { get; set; } = default;
+    public int Age { get; set; } = default;
+    public string Address { get; set; } = default;
+    public string Email { get; set; } = default;
+    public string Phone { get; set; } = default;
+}
---
+++ console-app/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using class_lib;
+
+Person person = new Person
+{
+    Name = "John Doe",
+    Age = 25,
+    Address = "123 Main St",
+    Email = "vamsi.doddapaneni@outlook.com",
+    Phone = "123-456-7890"
+};
+Console.WriteLine(person.Name);
+Console.WriteLine("Press any key to exit.");
+
+
